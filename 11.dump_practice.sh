07dcbb61af9dad86d5d52762c9242596e54feb08
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-02 09:30:34 +0900

    create 13.view_procedure

--- 11.dump_practice.sh
+++ 11.dump_practice.sh
@@ -1,9 +1,9 @@
 # 덤프파일 생성
 mysqldump -u root -p 스키마명 > 덤프파일명.sql
 mysqldump -u root -p board > mydumpfile.sql
-docker exec -it 10b7f5d05dcd mariadb-dump -u root -p1234 board > mydb_backup.sql
+docker exec -it 컨테이너ID mariadb-dump -u root -p1234 board > mydb_backup.sql
 
 # 덤프파일 적용(복원)
 mysql -u root -p 스키마명 < 덤프파일명
 mysql -u root -p board < mydumpfile.sql
-docker exec -i 10b7f5d05dcd mariadb -u root -p1234 board < mydb_backup.sql
+docker exec -i 컨테이너ID mariadb -u root -p1234 board < mydb_backup.sql
